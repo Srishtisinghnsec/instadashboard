b60c78746b17fc820bbb17bfb334082133584ec8
Srishtisinghnsec <116015199+Srishtisinghnsec@users.noreply.github.com>
2024-10-22 11:39:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    message = fetch_instagram_insights()  # Call the function from untitled49.py
+    username=input("enter your username")
+    password=input("enter your password")
+    message = fetch_instagram_insights(username,password)  # Call the function from untitled49.py
     return {"message": message}
